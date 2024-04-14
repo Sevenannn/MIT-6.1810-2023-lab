c58afc5a93513b9a8016c79325ebacd98cc4fb6c
Sevenannn <qianqliu@uw.edu>
2024-04-13 17:29:30 -0700

    speed up system calls

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  // usyscall structure of each process
+  struct usyscall *u;           // store pid of the current process
 };
