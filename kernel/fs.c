84f591283a0e6d2887e378164d19a556c53c537d
Sevenannn <qianqliu@uw.edu>
2024-06-29 12:40:26 -0700

    Finish Large Files

--- kernel/fs.c
+++ kernel/fs.c
@@ -416,6 +416,40 @@ bmap(struct inode *ip, uint bn)
     brelse(bp);
     return addr;
   }
+  bn -= NINDIRECT;
+
+  if(bn < NDOUBLEINDIRECT){
+    if((addr = ip->addrs[NDIRECT + 1]) == 0){
+      addr = balloc(ip->dev);
+      if(addr == 0)
+        return 0;
+      ip->addrs[NDIRECT + 1] = addr;
+    }
+    bp = bread(ip->dev,addr);
+    a = (uint*)bp->data;
+
+    if((addr = a[bn / NINDIRECT]) == 0){
+      addr = balloc(ip->dev);
+      if(addr){
+        a[bn / NINDIRECT] = addr;
+        log_write(bp);
+      }
+    }
+    brelse(bp);
+    
+    bp = bread(ip->dev,addr);
+    a = (uint*)bp->data;
+    if((addr = a[bn % NINDIRECT]) == 0){
+      addr = balloc(ip->dev);
+      if(addr){
+        a[bn % NINDIRECT] = addr;
+        log_write(bp);
+      }
+    }
+    brelse(bp);
+    
+    return addr;
+  }
 
   panic("bmap: out of range");
 }
@@ -425,9 +459,9 @@ bmap(struct inode *ip, uint bn)
 void
 itrunc(struct inode *ip)
 {
-  int i, j;
-  struct buf *bp;
-  uint *a;
+  int i, j, k;
+  struct buf *bp, *bp_l1, *bp_l2;
+  uint *a, *a_l1, *a_l2;
 
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
@@ -446,6 +480,26 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+  }
+
+  if(ip->addrs[NDIRECT + 1]){
+    bp_l1 = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+    a_l1 = (uint*)bp_l1->data;
+    for(j = 0; j < NINDIRECT; j++){
+      if(a_l1[j]){
+        bp_l2 = bread(ip->dev, a_l1[j]);
+        a_l2 = (uint*)bp_l2->data;
+        for (k = 0; k < NINDIRECT; k++) {
+          if(a_l2[k])
+            bfree(ip->dev, a_l2[k]);
+        }
+        brelse(bp_l2);
+        bfree(ip->dev, a_l1[j]);
+      }
+    }
+    brelse(bp_l1);
+    bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+    ip->addrs[NDIRECT + 1] = 0;
   }
 
   ip->size = 0;
